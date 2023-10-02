626d37437a09f0e7118b83621b91fff9a6cdb50a
DeFault <78411508+delefault@users.noreply.github.com>
2023-10-02 12:35:14 +0000

    Update main.py
    Added ability to ignore certain users, ability to track editing of messages.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyrogram.errors import UsernameNotOccupied
 from pyrogram import Client, filters
 from datetime import datetime
 import logging
@@ -13,7 +14,15 @@ def clear_console():
 def display_menu():
     print("1. Настройка api_id и api_hash")
     print("2. Настройка номера телефона")
-    print("3. Запустить скрипт")
+    print("3. Следить за редактированием сообщений (текущее состояние: {})".format(watch_edited_messages))
+    print("4. Запустить скрипт")
+
+def get_user_id_by_username(username):
+    try:
+        user = app.get_users(username)
+        return user.id
+    except UsernameNotOccupied:
+        return None
 
 def change_api_id():
     global api_id
@@ -34,10 +43,20 @@ def save_config():
     config['User'] = {
         'api_id': str(api_id),
         'api_hash': api_hash,
-        'phone_number': phone_number
+        'phone_number': phone_number,
+        'watch_edited_messages': str(watch_edited_messages),
+        'ignore_users': ', '.join(ignore_users)
     }
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
+
+def toggle_watch_messages():
+    global watch_edited_messages
+    watch_edited_messages = not watch_edited_messages
+    save_config()
+    print("Теперь следим за редактированием сообщений.".format(watch_edited_messages))
+
+
 
 config = configparser.ConfigParser()
 
@@ -46,15 +65,20 @@ if os.path.exists('config.ini'):
     api_id = config['User'].get('api_id', '')
     api_hash = config['User'].get('api_hash', '')
     phone_number = config['User'].get('phone_number', '')
+    watch_edited_messages = config['User'].getboolean('watch_messages', False)
+    ignore_users = config['User'].get('ignore_users', '').split(', ')
 else:
     api_id = ''
     api_hash = ''
     phone_number = ''
+    watch_edited_messages = False
+    ignore_users = []
+
 
 clear_console()
-while not api_id or not api_hash or not phone_number:
+while True:
     display_menu()
-    choice = input("Выберите опцию (1, 2 или 3): ")
+    choice = input("Выберите опцию (1, 2, 3 или 4): ")
     
     if choice == '1':
         change_api_id()
@@ -63,6 +87,9 @@ while not api_id or not api_hash or not phone_number:
         change_phone_number()
         clear_console()
     elif choice == '3':
+        toggle_watch_messages()
+        clear_console()
+    elif choice == '4':
         if not api_id or not api_hash or not phone_number:
             clear_console()
             print("\nПожалуйста, заполните все поля (api_id, api_hash и номер телефона) перед запуском скрипта.\n")
@@ -70,6 +97,7 @@ while not api_id or not api_hash or not phone_number:
             break
     else:
         input("Неверный выбор. Нажмите Enter для продолжения...")
+
 
 
 app = Client('SESSION_FOR_CONTROL_DELETEMSG2', api_id, api_hash, device_model="Pixel 3 XL", system_version="Android 10.0", phone_number = phone_number)
@@ -103,20 +131,30 @@ async def handle_private_message(client, message):
     message_id = message.id
     message_content = message.text or message.caption
     sender_id = message.from_user.id
-    sender_username = message.from_user.username
-    message_type = get_message_type(message)
-    
-    if sender_id != app.me.id:
-        log_message = f"[Новое сообщение в ЛС] ID: {message_id} | Отправитель: @{sender_username} ({sender_id}) | Тип сообщения: {message_type}"
-        print(Fore.GREEN + log_message + Style.RESET_ALL)
-        #logging.info(log_message)
-    
-    received_messages[message_id] = {
-        'content': message_content,
-        'sender_id': sender_id,
-        'sender_username': sender_username,
-        'message_type': message_type
-    }
+    if sender_id not in ignore_users:
+        sender_username = message.from_user.username
+
+        message_type = get_message_type(message)
+        
+        if sender_id != app.me.id:
+            log_message = f"[Новое сообщение в ЛС] ID: {message_id} | Отправитель: @{sender_username} ({sender_id}) | Тип сообщения: {message_type}"
+            print(Fore.GREEN + log_message + Style.RESET_ALL)
+            #logging.info(log_message)
+        
+        received_messages[message_id] = {
+            'content': message_content,
+            'sender_id': sender_id,
+            'sender_username': sender_username,
+            'message_type': message_type
+        }
+    else:
+        received_messages[message_id] = {
+            'content': None,
+            'sender_id': sender_id,
+            'sender_username': None,
+            'message_type': None
+        }
+
 
 @app.on_deleted_messages()
 async def handle_deleted_message(client, messages):
@@ -127,31 +165,64 @@ async def handle_deleted_message(client, messages):
         if message_id in received_messages:
             content = received_messages[message_id]['content']
             sender_id = received_messages[message_id]['sender_id']
-            sender_username = received_messages[message_id]['sender_username']
-            message_type = received_messages[message_id]['message_type']
-            
-            if sender_id == app.me.id:
-                textmsg = f"Ваше сообщение было удалено: {content}\nВремя удаления: {delete_time}\nТип сообщения: {message_type}"
-                await app.send_message("me", textmsg)
-                log_message = f"[Ваше сообщение было удалено] Время удаления: {delete_time} | Тип сообщения: {message_type}"
-                print(Fore.RED + log_message + Style.RESET_ALL)
-                #logging.info(log_message)
-            else:
-                textmsg = f"Удалено сообщение в ЛС:\nID: {message_id}\nСодержимое: {content}\nОтправитель: @{sender_username} ({sender_id})\nВремя удаления: {delete_time}\nТип сообщения: {message_type}"
-                await app.send_message("me", textmsg)
-                log_message = f"[Удалено сообщение в ЛС] ID: {message_id} | Отправитель: @{sender_username} ({sender_id}) | Время удаления: {delete_time} | Тип сообщения: {message_type}"
-                print(Fore.RED + log_message + Style.RESET_ALL)
-                #logging.info(log_message)
+            if sender_id not in ignore_users:
+                sender_username = received_messages[message_id]['sender_username']
+                message_type = received_messages[message_id]['message_type']
+                
+                if sender_id == app.me.id:
+                    textmsg = f"Ваше сообщение было удалено: {content}\nВремя удаления: {delete_time}\nТип сообщения: {message_type}"
+                    await app.send_message("me", textmsg)
+                    log_message = f"[Ваше сообщение было удалено] Время удаления: {delete_time} | Тип сообщения: {message_type}"
+                    print(Fore.RED + log_message + Style.RESET_ALL)
+                    #logging.info(log_message)
+                else:
+                    textmsg = f"Удалено сообщение в ЛС:\nID: {message_id}\nСодержимое: {content}\nОтправитель: @{sender_username} ({sender_id})\nВремя удаления: {delete_time}\nТип сообщения: {message_type}"
+                    await app.send_message("me", textmsg)
+                    log_message = f"[Удалено сообщение в ЛС] ID: {message_id} | Отправитель: @{sender_username} ({sender_id}) | Время удаления: {delete_time} | Тип сообщения: {message_type}"
+                    print(Fore.RED + log_message + Style.RESET_ALL)
+                    #logging.info(log_message)
         else:
             log_message = f"[Удалено сообщение] ID: {message_id} | Информация о сообщении отсутствует | Время удаления: {delete_time}"
             print(Fore.RED + log_message + Style.RESET_ALL)
             # logging.info(log_message)
             pass
 
+if watch_edited_messages:
+    @app.on_edited_message()
+    async def handle_edited_message(client, messages):
+        message_id = messages.id
+        delete_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        if message_id in received_messages:
+            content = received_messages[message_id]['content']
+            content_new = messages.text or messages.caption
+            sender_id = received_messages[message_id]['sender_id']
+            if sender_id not in ignore_users:
+                sender_username = received_messages[message_id]['sender_username']
+                message_type = received_messages[message_id]['message_type']
+                
+                if sender_id == app.me.id:
+                    pass
+                else:
+                    textmsg = f"Отредактировано сообщение в ЛС:\nID: {message_id}\nБыло: {content}\nСтало: {content_new}\nОтправитель: @{sender_username} ({sender_id})\nВремя редактирования: {delete_time}\nТип сообщения: {message_type}"
+                    await app.send_message("me", textmsg)
+                    log_message = f"[Отредактировано сообщение в ЛС] ID: {message_id} | Отправитель: @{sender_username} ({sender_id}) | Время редактирования: {delete_time} | Тип сообщения: {message_type}"
+                    print(Fore.RED + log_message + Style.RESET_ALL)
+                    #logging.info(log_message)
+        else:
+            log_message = f"[Отредактировано сообщение] ID: {message_id} | Информация о сообщении отсутствует | Время редактирования: {delete_time}"
+            print(Fore.RED + log_message + Style.RESET_ALL)
+            # logging.info(log_message)
+            pass
+
+
+
 clear_console()
 print("""
 ▄▀█ █▄░█ ▀█▀ █ █▀▄ █▀▀ █░░ █▀▀ ▀█▀ █▀▀ █▀█ █▀▄▀█ █▀ █▀▀|ᵇʸ ᵈᵉˡᵃᶠᵃᵘˡᵗ
 █▀█ █░▀█ ░█░ █ █▄▀ ██▄ █▄▄ ██▄ ░█░ ██▄ █▀▄ █░▀░█ ▄█ █▄█
       FOR TELEGRAM (version 0.1)\n\n""")
+ignore_users = [int(user_id) for user_id in ignore_users]
+
 
 app.run()
